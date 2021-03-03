2e401afdd5274f4b13e527a2fc2730c1cfd84f16
sapir.c <sapir.c@SAPIRCOH-LT-IL>
2021-03-03 16:44:31 +0200

    Add pages

--- client/user/src/components/header.js
+++ client/user/src/components/header.js
@@ -11,10 +11,10 @@ export default function Header(){
         <header className="w3-display-container w3-wide" id="home">
           <img className="w3-image" src="images/showroom.png" alt="Fashion Blog" width={1600} height={1060} />
           <div className="w3-display-left w3-padding-large">
-            <h6><button className="w3-button w3-black w3-padding-large w3-large w3-hover-opacity-off" Link to="/women">SHOP WOMEN</button></h6>
+            <Link to='/Women'><h6><div className="w3-button w3-black w3-padding-large w3-large w3-hover-opacity-off">SHOP WOMEN</div></h6></Link>
           </div>
           <div className="w3-display-right w3-padding-large">
-            <h6><button className="w3-button w3-black w3-padding-large w3-large w3-hover-opacity-off">SHOP MAN</button></h6>
+            <Link to='/Men'><h6><div className="w3-button w3-black w3-padding-large w3-large w3-hover-opacity-off">SHOP MEN</div></h6></Link>
           </div>
         </header>
       </div>
