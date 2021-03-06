a2215f61a13b62807bcdd9782d53bffad3337925
danielkaneti <danielle7654@gmail.com>
2021-03-06 10:32:58 +0200

    register

--- client/user/src/pages/register/Register.js
+++ client/user/src/pages/register/Register.js
@@ -1,28 +1,66 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import history from '../../History';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
 import "./Register.css";
 
-function Register() {
- 
+function Register()  {
+  const [firstNameInput, setFirstNameInput] = useState('');
+  const [lastNameInput, setLastNameInput] = useState('');
+  const [usernameInput, setUsernameInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+
+
+  // const [isAuth, setIsAuth] = useState(true);
+  // useEffect(() => {
+  //     if (!isAuth) {
+  //         alert('this email is alreay registered');
+  //         setIsAuth(true);
+  //     }
+  // }, [isAuth])
+
+  const onSubmit = data => {
+      axios.post('http://localhost:2222/users', data)
+          .then(response => {
+              alert('user created')
+              history.push('/')
+          })
+  }
+  
   return (
+    
 <div>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
-  {/*---- Include the above in your HEAD tag --------*/}
+
   <div className="wrapper fadeInDown">
     <div id="formContent">
-      {/* Tabs Titles */}
-      {/* Icon */}
+   
       <div className="fadeIn first">
      
       </div>
-      {/* Login Form */}
-      <form>
+    
+      <form  >
       <h3>Register</h3>
-      <input type="text" id="username" className="fadeIn second" name="user name" placeholder="user name" />
-      <input type="text" id="firstname" className="fadeIn second" name="First name" placeholder="First name" />
-      <input type="text" id="lastname" className="fadeIn second" name="Last name" placeholder="Last name" />
-        <input type="text" id="login" className="fadeIn second" name="Email" placeholder="Email" />
-        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
-        <input type="submit" className="fadeIn fourth" defaultValue="Log In" />
+      <input type="text" id="username" 
+      className="fadeIn second" name="username" 
+      placeholder="user name" 
+      onChange={(event)=>{setUsernameInput(event.target.value);}}/>
+      <input type="text" id="firstname" className="fadeIn second"
+       name="First name" placeholder="First name"
+       onChange={(event)=>{setFirstNameInput(event.target.value);}} />
+      <input type="text" id="lastname" className="fadeIn second" 
+      name="Last name" placeholder="Last name"
+       onChange={(event)=>{setLastNameInput(event.target.value);}} />
+        <input type="text" id="email" className="fadeIn second" 
+        name="Email" placeholder="Email"
+         onChange={(event)=>{setEmailInput(event.target.value);}} />
+        <input type="text" id="password" className="fadeIn third" 
+        name="login" placeholder="password"
+         onChange={(event)=>{setPasswordInput(event.target.value);}} />
+     
+          <input type="submit" className="fadeIn fourth" defaultValue="Log In"/>
+ 
       </form>
      
     </div>
