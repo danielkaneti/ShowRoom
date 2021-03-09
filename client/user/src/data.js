41b4ff16d3dc6fc386388301e16aa8d2ad29d381
sapir.c <sapir.c@SAPIRCOH-LT-IL>
2021-03-09 19:45:12 +0200

    Create catalog and signin pages

---
+++ client/user/src/data.js
@@ -0,0 +1,14 @@
+export const wineList = [
+    {
+        title: 'wine 1',
+        type: 'A',
+    }, 
+    {
+        title: 'wine 2',
+        type: 'B',
+    }, 
+    {
+        title: 'wine 3',
+        type: 'C',
+    }
+]
