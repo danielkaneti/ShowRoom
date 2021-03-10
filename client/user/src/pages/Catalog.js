e50732051a4ed89043ac4a01c430140cff8065e5
sapir.c <sapir.c@SAPIRCOH-LT-IL>
2021-03-10 15:50:14 +0200

    Add product from mongo

--- client/user/src/pages/Catalog.js
+++ client/user/src/pages/Catalog.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Component } from 'react';
 import { Container} from '../components/GeneralStyle';
 import WineCard from '../components/WineCard';
 import {wineList} from '../data';
 import styled from 'styled-components';
+import axios from 'axios';
+
+  
 
 const Catalog = () => {
 
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        axios.get("http://localhost:2222/products").then(resp => setProducts(resp.data))}, []);
+
     return(
+        
         <Container>
             <Grid>
-                {wineList.map(wine => (
+                {products.map(wine => (
                     <WineCard wine={wine}/>
                 ))}
             </Grid>
         </Container>
     );
 }
+
 
 const Grid = styled.div`
   display: flex;
