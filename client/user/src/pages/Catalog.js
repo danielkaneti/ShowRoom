6ea53e197d35aa187962270d4ece527410c6343b
sapir.c <sapir.c@SAPIRCOH-LT-IL>
2021-03-11 21:05:04 +0200

    test

--- client/user/src/pages/Catalog.js
+++ client/user/src/pages/Catalog.js
@@ -7,21 +7,43 @@ import styled from 'styled-components';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import {productsURL} from '../api/wine';
+import SearchInput from '../components/SearchInput';
 
 
 const Catalog = () => {
 
     const [products, setProducts] = useState([]);
+    const [titleInput, setTitleInput] = useState("");
+    const [genreInput, setGenreInput] = useState("");
+    const [yearInput, setYearInput] = useState("");
+    const handleTitleInput = (e) => {
+        setTitleInput(e.target.value)
+      };
     
+      const handleGenreInput = (e) => {
+        setGenreInput(e.target.value)
+      };
+    
+      const handleYearInput = (e) => {
+        setYearInput(e.target.value)
+      };
     useEffect(() => {
         axios.get(productsURL())
         .then(resp => setProducts(resp.data))
     }, []);
 
     return(
+
+        
         <Backgroung>
+            <StyledSearchDiv>
+                <SearchInput placeholder="Title..." onChange={handleTitleInput} />
+                <SearchInput placeholder="Genre..." onChange={handleGenreInput} />
+                <SearchInput placeholder="Year..." onChange={handleYearInput} />
+            </StyledSearchDiv>
             <Container>
                 <Grid>
+                
                     {products.map(wine => (
                         <NavLink to={`/catalog/${wine._id}`}>
                             <WineCard wine={wine}/>
@@ -51,6 +73,16 @@ const Backgroung = styled.div`
 
 const NavLink = styled(Link)`
     text-decoration: none;
+`;
+
+
+const StyledSearchDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #d9d9d9;
+  padding: 3rem;
+  margin-bottom: 2rem;
 `;
 
 export default Catalog;
